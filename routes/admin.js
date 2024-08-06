302f4e4269395c525048591709f987c66329d03c
TarunShyam001 <shyamtarun2001@gmail.com>
2024-08-06 23:07:44 +0530

    filter is applied and task03 is completed

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET Request
 router.get('/add-product',(req, res, next)=>{
     res.send(`
         <html>
             <body>
-                <form action="/product" method="POST">
+                <form action="/admin/add-product" method="POST">
                     <input type="text" name="title">
                     <input type="number" min="0" name="size">
                     <button type="submit"> Add Product </button>
@@ -16,7 +17,8 @@ router.get('/add-product',(req, res, next)=>{
     );
 });
 
-router.post('/product', (req, res, next)=>{
+// /admin/add-product => POST Request
+router.post('/add-product', (req, res, next)=>{
     console.log(req.body.title);
     console.log(req.body.size);
     res.redirect('/');
