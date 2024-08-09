1565f5f168a01d351c03d360563ff906cce9d841
TarunShyam001 <shyamtarun2001@gmail.com>
2024-08-09 14:08:13 +0530

    Task 5 begins from here

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//switching the html file in views
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -6,13 +7,14 @@ const app = express();
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
+
 app.use(bodyParser.urlencoded({extended : false}));
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next)=>{
-    res.status(404).send('<h1> Page not found </h1>'); 
+    res.status(404).send('<h1> Page not found here </h1>'); 
 })
 
 const PORT = 2300;
